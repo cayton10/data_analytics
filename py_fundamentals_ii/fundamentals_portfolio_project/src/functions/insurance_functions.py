b164238ed670614ab028bc9430de0ce7a0668449
Cayton <cayton10@live.marshall.edu>
2023-02-17 12:51:21 -0500

    Smoker percent difference in premiums

--- py_fundamentals_ii/fundamentals_portfolio_project/src/functions/insurance_functions.py
+++ py_fundamentals_ii/fundamentals_portfolio_project/src/functions/insurance_functions.py
@@ -2,8 +2,9 @@ import csv
 import json
 import math
 import logging
+from typing import Union
 
-def find_average_age_total(data: list, key: str):
+def find_average(data: list, key: str):
     """
     Calculates the average for a key value set in list
 
@@ -17,12 +18,60 @@ def find_average_age_total(data: list, key: str):
     try:
         for record in data:
             if key in record.keys():
-                    total_num += int(record[key])
+                # This limits the versatility of this mod, but I'm over achieving for this project already
+                    if not key == 'age':
+                        total_num = float(total_num)
+                        total_num += float(record[key])
+                    else:
+                        total_num += int(record[key])
             else:
                  raise KeyError('Invalid key for this operation. Check dictionary keys.')
+    except KeyError as k:
+        return k
+    except Exception as e:
+        logging.exception(f"An error occurred while calculating the desired average: {e}")
+    
+    return total_num / num_records
+
+
+def extract_records_by_key(data: list(dict()), key: str, value: Union[str, int, bool]):
+    """
+    Extracts records from the list based on the value of the key supplied in the args.
+
+    Parameters
+    ----------
+    data : list
+    List of dictionary objects
+    key : string
+    Key supplied by the user to extract a record into a new list for return
+    value: bool | int | string
+    Value checked for extracting records into new list.
+    
+    
+    Raises
+    ------
+    TypeError
+    If incorrect arg types are passed, throws TypeError
+
+    KeyError
+    If the user provided key does not exist in the dictionary object, throws KeyError
+    
+    Exception
+    Generic exception
+    """
+    ret_list = []
+    try:
+        for record in data:
+            if key in record.keys():
+                if value == record[key]:
+                    ret_list.append(record)
+                else:
+                    continue
+            else:
+                raise KeyError('Invalid key for this operation. Check dictionary keys.')
     except KeyError:
         return KeyError
     except Exception:
-        logging.exception(f"An error occurred while calculating the desired average: {Exception}")
-    
-    return total_num / num_records
+        logging.exception(f"An error occurred while calculating the desire average: {Exception}")
+
+    return ret_list
--- py_fundamentals_ii/fundamentals_portfolio_project/src/functions/insurance_functions_test.py
+++ py_fundamentals_ii/fundamentals_portfolio_project/src/functions/insurance_functions_test.py
@@ -1,34 +1,81 @@
 import unittest
 
-from .insurance_functions import find_average_age_total
+import functions.insurance_functions as test_funcs 
 
 class TestAverage(unittest.TestCase):
     # Success case
     def test_find_average_age_total_success(self):
         # Create mock data for success case
         key = "age"
-        success_list = create_mock_list(key)
-        result = find_average_age_total(success_list, key)
+        success_list = create_mock_avg_list_integers(key)
+        result = test_funcs.find_average(success_list, key)
         self.assertEqual(result, 4.5)
     
     # Failure case
     def test_find_average_age_total_key_error_exception(self):
         key = "age"
         new_key = "bogus"
-        failure_list = create_mock_list(new_key)
-        result = find_average_age_total(failure_list, key)
+        failure_list = create_mock_avg_list_integers(new_key)
+        result = test_funcs.find_average(failure_list, key)
+        self.assertRaises(KeyError)
+    
+    # Calculate the averages for float type string inputs
+    def test_find_average_cost_success(self):
+        key = "cost"
+        success_list = create_mock_avg_list_float(key)
+        print(success_list)
+        result = test_funcs.find_average(success_list, key)
+        self.assertEqual(result, 1000.3)
+
+    # Begin record extraction testing methods
+    #
+    # Success Case
+    def test_extract_records_by_key_success(self):
+        # Create mock data for success case
+        key = "smoker"
+        value = "yes"
+        mock_list = []
+        for i in range(0, 10):
+            if i % 2 == 0:
+                mock_list.append(create_mock_extract_dict(key, value))
+            else:
+                mock_list.append(create_mock_extract_dict(key, "no"))
+
+        result = test_funcs.extract_records_by_key(mock_list, key, value)
+        self.assertEqual(len(result), 5)
+    # Failure case
+    def test_extract_records_by_key_raises_key_error(self):
+        key = "smoker"
+        value = "yes"
+        mock_list = []
+        for i in range(0, 10):
+            mock_list.append(create_mock_extract_dict(key, value))
+        result = test_funcs.extract_records_by_key(mock_list, "bogus", value)
         self.assertRaises(KeyError)
 
 if __name__ == '__main__':
     unittest.main()
-
-
+        
 
 # Helper methods
-def create_mock_list(key: str):
+def create_mock_avg_list_integers(key: str):
     mock_list = []
     for i in range(0, 10):
         temp_dict = dict()
         temp_dict.update({key: str(i)})
         mock_list.append(temp_dict)
     return mock_list
+
+def create_mock_avg_list_float(key: str):
+    mock_list = []
+    for i in range(0, 10):
+        temp_dict = dict()
+        val = round((i / 15) + 1000, 2)
+        temp_dict.update({key: str(val)})
+        mock_list.append(temp_dict)
+    return mock_list
+
+def create_mock_extract_dict(key: str, value: bool | int | str):
+    mock_dict = dict()
+    mock_dict.update({key: value})
+    return mock_dict
